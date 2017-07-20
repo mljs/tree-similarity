5080dc83a7f0c17661d63d6548bb2f93bc707986
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2017-07-20 17:12:22 +0200

    test: fix imports

--- src/__tests__/tree.js
+++ src/__tests__/tree.js
@@ -1,27 +1,22 @@
-import tree from '..';
+import treeSimilarity, {createTree} from '..';
+
+var a = [
+    [1, 2, 3, 4, 5, 6, 7],
+    [0.3, 0.7, 4, 0.3, 0.2, 5, 0.3]
+];
+var b = [
+    [1, 2, 3, 4, 5, 6, 7],
+    [0.3, 4, 0.7, 0.3, 5, 0.2, 0.3]
+];
 
 describe('Tree similarity', () => {
-
-    var a, b;
-    beforeEach(() => {
-        a = [
-            [1, 2, 3, 4, 5, 6, 7],
-            [0.3, 0.7, 4, 0.3, 0.2, 5, 0.3]
-        ];
-        b = [
-            [1, 2, 3, 4, 5, 6, 7],
-            [0.3, 4, 0.7, 0.3, 5, 0.2, 0.3]
-        ];
-    });
-
     it('should work with two arrays', () => {
-        expect(tree(a, b)).toBeCloseTo(0.685253, 4);
+        expect(treeSimilarity(a, b)).toBeCloseTo(0.685253, 4);
     });
 
     it('should create Tree object', () => {
-        var theTree = tree.createTree(a);
-        expect(theTree.center).toBeCloseTo(4.5, 4);
-        expect(theTree.sum).toEqual(10.2);
+        var tree = createTree(a);
+        expect(tree.center).toBeCloseTo(4.5, 4);
+        expect(tree.sum).toBeCloseTo(10.2, 4);
     });
-
 });
