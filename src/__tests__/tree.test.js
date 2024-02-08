ec9318f470600cf028476ef7b37ed08c3560587d
Jose Alejandro Bolanos Arroyave <jose.bolanos@correounivalle.edu.co>
2024-02-08 11:05:13 -0500

    refactor: change exposed methods and accepted types
    refactor!: treeSimilarity now only accepts Tree objects
    
    refactor!: createTree expects an object {x: [],y: []}

--- src/__tests__/tree.test.js
+++ src/__tests__/tree.test.js
@@ -1,28 +1,21 @@
 import { describe, it, expect } from 'vitest';
 
-import { treeSimilarity, getFunction } from '..';
+import { createTree, treeSimilarity } from '../index';
 
-let a = [
-  [1, 2, 3, 4, 5, 6, 7],
-  [0.3, 0.7, 4, 0.3, 0.2, 5, 0.3],
-];
-let b = [
-  [1, 2, 3, 4, 5, 6, 7],
-  [0.3, 4, 0.7, 0.3, 5, 0.2, 0.3],
-];
+const a = {
+  x: [1, 2, 3, 4, 5, 6, 7],
+  y: [0.3, 0.7, 4, 0.3, 0.2, 5, 0.3],
+};
+const b = {
+  x: [1, 2, 3, 4, 5, 6, 7],
+  y: [0.3, 4, 0.7, 0.3, 5, 0.2, 0.3],
+};
 
 describe('Tree similarity', () => {
   it('should work with two arrays', () => {
-    expect(treeSimilarity(a, b)).toBeCloseTo(0.653354, 4);
-  });
-
-  it('should currify the options', () => {
-    let options = {
-      alpha: 0.4,
-      beta: 0.5,
-      gamma: 0.001,
-    };
-    let func = getFunction(options);
-    expect(func(a, b)).toBe(treeSimilarity(a, b, options));
+    expect(treeSimilarity(createTree(a), createTree(b))).toBeCloseTo(
+      0.653354,
+      4,
+    );
   });
 });
